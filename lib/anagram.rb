95e53d2ac547d21864520fb18c2275acf883968f
Alexander Gould <alexander.gould94@gmail.com>
2021-02-17 15:37:59 -0800

    commit tree 563dc599ecf343081f2e6abd58f7dc8b87e61967

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    attr_accessor :word
+    
+    def initialize(word)
+        @word = word
+        @split_word = word.split('')
+    end
+
+    def match(anagrams)
+        @matches = []
+        anagrams.each do |possible_anagram|
+            @possible_matches = possible_anagram.split('')
+            if @possible_matches.sort == @split_word.sort
+                @matches.push(possible_anagram)
+            end
+        end
+        return @matches
+    end
+end
+
+#     if @split_anagram.sort == @split_word.sort
+    #         @matches.push(possible_anagram)
+    #     else
+    #         return []
+    #     end 
+    # @matches
